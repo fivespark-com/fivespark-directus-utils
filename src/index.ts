9c12784f9802e33b98f3c79b623105dd33c8b652
Ewout Stortenbeker <me@appy.one>
2024-05-31 14:51:30 +0200

    copy and export types from dist dir

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './directus.js';
 export * from './minimal-upsert.js';
-export type * from './types/select.d.ts';
+export type * from './types/select.js';
 export * from './errors.js';
 export * from './http-status-codes.js';
 export * from './thread-safe.js';
